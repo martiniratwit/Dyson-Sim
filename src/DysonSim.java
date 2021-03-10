c77a48dd5c7cdc992c7f1b749c77b20c29d8f3ab
Robert Martini <rob.martini@outlook.com>
2021-03-10 08:44:44 -0500

    Background of 2D sun, mercury, and stars.

---
+++ src/DysonSim.java
@@ -0,0 +1,10 @@
+
+public class DysonSim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MyFrame();
+
+	}
+
+}
---
+++ src/MyPanel.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MyPanel extends JPanel {
+
+    final int PANEL_WIDTH = 600;
+    final int PANEL_HEIGHT = 500;
+
+    Image backroundImage;
+
+    MyPanel() {
+
+        this.setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_HEIGHT));
+        backroundImage = new ImageIcon("Backround.jpg").getImage();
+
+
+    }
+
+    public void paint(Graphics g) {
+        Graphics g2D =  (Graphics2D) g;
+
+        g2D.drawImage(backroundImage,0,0,null);
+    }
+}
