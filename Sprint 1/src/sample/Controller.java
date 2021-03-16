752e49c5116fa761b9b3a10e9b37c5b72869ac01
Seamus Nauton <47990658+nautonsatwit@users.noreply.github.com>
2021-03-16 13:47:18 -0400

    Add files via upload

---
+++ Sprint 1/src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+/*
+        A = Solar Panel Cost (10$per sq ft)
+        B = Square Footage ~500
+        C = Production cost (constant 1mil per)
+        D = Fuel Cost(1.65$ per gallon 3ton 770g/t)
+        E = Launch Cost (Constant 20mil)
+*/
+
+public class Controller {
+}
